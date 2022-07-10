4503860ab646495b54d412911bdd078609f72235
rconsoleprint enthusiast <83344789+LuaConsole@users.noreply.github.com>
2022-07-10 20:55:32 +0100

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,60 @@
+while true do
+
+wait(10)
+
+local chat = math.random(1, 50)
+
+-- game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("test", "All")
+
+if chat = 1 then game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Today is a nice day!", "All")
+if chat = 2 then game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Endo is a really cool person.", "All")
+if chat = 3 then
+if chat = 4 then
+if chat = 5 then
+if chat = 6 then
+if chat = 7 then
+if chat = 8 then
+if chat = 9 then
+if chat = 10 then
+if chat = 11 then
+if chat = 12 then
+if chat = 13 then
+if chat = 14 then
+if chat = 15 then
+if chat = 16 then
+if chat = 17 then
+if chat = 18 then
+if chat = 19 then
+if chat = 20 then
+if chat = 21 then
+if chat = 22 then
+if chat = 23 then
+if chat = 24 then
+if chat = 25 then
+if chat = 26 then
+if chat = 27 then
+if chat = 28 then
+if chat = 29 then
+if chat = 30 then
+if chat = 31 then
+if chat = 32 then
+if chat = 33 then
+if chat = 34 then
+if chat = 35 then
+if chat = 36 then
+if chat = 37 then
+if chat = 38 then
+if chat = 39 then
+if chat = 40 then
+if chat = 41 then
+if chat = 42 then
+if chat = 43 then
+if chat = 44 then
+if chat = 45 then
+if chat = 46 then
+if chat = 47 then
+if chat = 48 then
+if chat = 49 then
+if chat = 50 then
+
+end
